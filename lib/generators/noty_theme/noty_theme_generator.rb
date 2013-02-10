1a0c51bc9a96b0454b50d25bf642b061007480df
Shanky <shanky@shanky-Lenovo-G560.(none)>
2013-02-10 14:21:30 +0530

    update generator file according to rails 3

--- lib/generators/noty_theme/noty_theme_generator.rb
+++ lib/generators/noty_theme/noty_theme_generator.rb
@@ -1,9 +1,10 @@
-class  NotyThemeGenerator < Rails::Generator::Base
-	def manifest
-		record do |m|
-			m.file "jquery.noty.js","app/assets/javascripts/jquery.noty.js"
-			m.file "jquery.noty.css","app/assets/stylesheets/jquery.noty.css"
-			m.file "noty_theme_default.css","app/assets/stylesheets/noty_theme_default.css"
-		end
+class  NotyThemeGenerator < Rails::Generators::Base
+	source_root File.expand_path("../templates", __FILE__)
+  desc "This generator installs noty to Asset Pipeline"
+
+	def add_assets
+		copy_file "jquery.noty.js", "app/assets/javascripts/jquery.noty.js"
+		copy_file "jquery.noty.css", "app/assets/stylesheets/jquery.noty.css"
+		copy_file "noty_theme_default.css", "app/assets/stylesheets/noty_theme_default.css"
 	end
 end
